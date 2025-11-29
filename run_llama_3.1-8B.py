01823a595904ccb31855f371a2b4413124b810c5
Yi Situ <situdotdev@gmail.com>
2025-11-28 16:18:18 -0800

    Update run_llama_3.1-8B.py to remove warning about dtype

--- run_llama_3.1-8B.py
+++ run_llama_3.1-8B.py
@@ -8,7 +8,7 @@ import sys
 model_id = "meta-llama/Llama-3.1-8B"
 
 pipeline = transformers.pipeline(
-    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+    "text-generation", model=model_id, model_kwargs={"dtype": torch.bfloat16}, device_map="auto"
 )
 
 if len(sys.argv) < 2:
